1cec72a780bbce1a642bcbf13f99a7c6cdbff483
/ </>
2017-08-03 12:58:55 +0800

    improve

--- app/script/edit.js
+++ app/script/edit.js
@@ -1,6 +1,7 @@
 console.log('ask me to change');
 import Vue from 'vue';
 import axios from 'axios';
+import qs from 'qs';
 require('./../css/edit.scss');
 
 let edit = new Vue({
@@ -16,20 +17,13 @@ let edit = new Vue({
       mounted(){
            var self = this;
            axios.get('http://www.mytest.com/index/index/edit',{
-             
+                  params:{
+                      'dd': 'ddd',
+                  }
              })
               .then(function(item){
                   var data = item.data;
-                  self.bigSelects = data.bigtype;
-                  self.smalltypes = data.smalltype;
-                  self.bigselect = self.bigSelects[0].bigtype;
-                   console.log(self.smalltypes);
-                  for(let dom in self.smalltypes){
-                      if(self.smalltypes[dom].bigtype == self.bigselect){
-                          self.smallselect = self.smalltypes[0].type;
-                         console.log(self.smalltypes[dom]);
-                      }
-                  }
+                  self.setAllselect(data);
             });
       },
       methods:{
@@ -39,6 +33,39 @@ let edit = new Vue({
               console.log('text:'+self.text);
               console.log('bigselect:'+self.bigselect);
               console.log('smalltypes:'+self.smallselect);
+              axios.post('http://www.mytest.com/index/index/edit',qs.stringify({
+                    title: self.title,
+                    content: self.text,
+                    bigtype: self.bigselect,
+                    type: self.smallselect,
+                }))
+                .then(function(res){
+                console.log(res);
+                })
+                .catch(function(err){
+                console.log(err);
+                });
+          },
+          changebingselect(item){
+             var self = this;
+             for(let dom in self.smalltypes){
+                      if(self.smalltypes[dom].bigtype == item){
+                          self.smallselect = self.smalltypes[0].value;
+                      }
+              }
+          },
+          setAllselect(data){
+                  var self = this;
+                  self.bigSelects = data.bigtype;
+                  self.smalltypes = data.smalltype;
+                  self.bigselect = self.bigSelects[0].bigtype;
+                  console.log(self.smalltypes);
+                  for(let dom in self.smalltypes){
+                      if(self.smalltypes[dom].bigtype == self.bigselect){
+                          self.smallselect = self.smalltypes[0].value;
+                         console.log(self.smalltypes[dom]);
+                      }
+                  }
           }
       }
 });
