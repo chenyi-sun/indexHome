32e8ea5dc397446bdfb43a6dc59585f18738ab76
/ </>
2017-08-09 11:18:20 +0800

    improve indexhome

--- scrollmagictest/scrolldemo/js/controlscroll.js
+++ scrollmagictest/scrolldemo/js/controlscroll.js
@@ -1,7 +1,16 @@
  require(['ScrollMagic','debug','jquery','ScrollMagicJquery','TweenLite'], function(ScrollMagic,debug,$,ScrollMagicJquery,TweenLite){
             var controller = new ScrollMagic.Controller();
+            // var scene = new ScrollMagic.Scene({triggerElement: ".flag1"})
+            // .setClassToggle(".demo1", "scale")
+            // .addIndicators({name: "1 - add a class"})
+            // .addTo(controller);
+            function updateBox(){
+                console.log('sssssss');
+            }
             var scene = new ScrollMagic.Scene({triggerElement: ".flag1"})
-            .setClassToggle(".demo1", "scale")
-            .addIndicators({name: "1 - add a class"})
+            .setPin('.demo1')
+            // .on("enter leave", updateBox)
+            // .setClassToggle(".demo1", "scale")
+            .addIndicators({name: "1 ssss"})
             .addTo(controller);
         });
