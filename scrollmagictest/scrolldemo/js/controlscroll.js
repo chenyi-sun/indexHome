aa03f1593f942b7e35dc53ac11c4ebc8d9766946
/ </>
2017-08-10 19:39:09 +0800

    improve

--- scrollmagictest/scrolldemo/js/controlscroll.js
+++ scrollmagictest/scrolldemo/js/controlscroll.js
@@ -5,22 +5,33 @@
             // .addIndicators({name: "1 - add a class"})
             // .addTo(controller);
             function updateBox(){
-                console.log('sssssss');
+                
             }
-            var wipeAnimation = new TimelineMax()
-            .fromTo(".demo2", 1, {x: "-100%",top: '0%'}, {x: "0%", ease: Linear.easeNone});
-           
-
-            var scene = new ScrollMagic.Scene({
+            var wipeAnimation1 = new TimelineMax()
+            .fromTo(".demo1", 1, {x: "0%",top: '0%'}, {x: "0%",top:'0%', ease: Linear.easeNone});
+            var scene1 = new ScrollMagic.Scene({
                 triggerElement: ".flag1",
-                duration: "100%",
-                triggerHook: "onEnter",
+                duration: "300%",
+                triggerHook: "onLeave",
             })
             .setPin('.demo1')
-            .setPin('.demo2')
             // .on("enter leave", updateBox)
             // .setClassToggle(".demo1", "scale")
             .addIndicators({name: "1 name ssss"})
-            .setTween(wipeAnimation)
+            .setTween(wipeAnimation1)
+            .addTo(controller);
+
+             var wipeAnimation3 = new TimelineMax()
+            .fromTo(".demo3", 1, {top: '-100%'}, {top:'48.5%', ease: Linear.easeNone});
+            var scene1 = new ScrollMagic.Scene({
+                triggerElement: ".flag1",
+                duration: "300%",
+                triggerHook: "onLeave",
+            })
+            .setPin('.demo3')
+            // .on("enter leave", updateBox)
+            // .setClassToggle(".demo1", "scale")
+            .addIndicators({name: "3 name ssss"})
+            .setTween(wipeAnimation3)
             .addTo(controller);
         });
