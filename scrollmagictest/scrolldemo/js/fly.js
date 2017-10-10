849a7336ee87a6bea70f0ddca1febc5a2e64dc20
/ </>
2017-10-10 17:26:38 +0800

    add

---
+++ scrollmagictest/scrolldemo/js/fly.js
@@ -0,0 +1,4 @@
+ require(['ScrollMagic','debug','jquery','ScrollMagicJquery','TimelineMax','ScrollMagicGSAP'], function(ScrollMagic,debug,$,ScrollMagicJquery,TimelineMax){
+    var controller = new ScrollMagic.Controller();
+
+});
