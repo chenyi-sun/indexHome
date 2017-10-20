aa0e9a2c4b30e387be3159962475de9209cb02aa
/ </>
2017-10-20 17:18:28 +0800

    add

--- scrollmagictest/home/all/js/controlscroll.js
+++ scrollmagictest/home/all/js/controlscroll.js
@@ -1,5 +1,6 @@
  require(['ScrollMagic','debug','jquery','ScrollMagicJquery','TimelineMax','common','ScrollMagicGSAP'], function(ScrollMagic,debug,$,ScrollMagicJquery,TimelineMax,common){
         	 document.querySelector('#app').innerHTML = MyApp.templates.Loading();
 			//   document.querySelector('#app').innerHTML = MyApp.templates.App();
+			
 			 console.log('ssssssssss');
         });
--- scrollmagictest/home/js/templates.js
+++ scrollmagictest/home/js/templates.js
@@ -1,8 +1,8 @@
 this["MyApp"] = this["MyApp"] || {};
 this["MyApp"]["templates"] = this["MyApp"]["templates"] || {};
 this["MyApp"]["templates"]["App"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"hbs\">\r\n    <a>\r\n        give me some allowance\r\n    </a>\r\n</div>";
+    return "<div class=\"loading\">\r\n    <a>\r\n        give me some allowance\r\n    </a>\r\n</div>";
 },"useData":true});
 this["MyApp"]["templates"]["Loading"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<link href=\"css/loading.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n<div class=\"hbs\">\r\n    <a>\r\n        give me some allowance\r\n    </a>\r\n</div>";
+    return "<link href=\"css/loading.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n<div class=\"loading\">\r\n    <div class=\"loading_box\"></div>\r\n    <a>\r\n        give me some allowance\r\n    </a>\r\n</div>";
 },"useData":true});
