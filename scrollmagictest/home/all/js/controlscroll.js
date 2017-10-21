f0a2dc02636c88ec0c4fa170a1b1c9360db72476
/ </>
2017-10-21 14:29:27 +0800

    add improve

--- scrollmagictest/home/all/js/controlscroll.js
+++ scrollmagictest/home/all/js/controlscroll.js
@@ -1,7 +1,6 @@
  require(['ScrollMagic','debug','jquery','ScrollMagicJquery','TimelineMax','common','platform','ScrollMagicGSAP'], function(ScrollMagic,debug,$,ScrollMagicJquery,TimelineMax,common,platform){
         	document.querySelector('#app').innerHTML = MyApp.templates.Loading();
-			//   document.querySelector('#app').innerHTML = MyApp.templates.App();
+		//   document.querySelector('#app').innerHTML = MyApp.templates.App();
 		platform.init();
-		// console.log($('.not-tablet'));
-		// console.log('ssssssssss');
+		
         });
--- scrollmagictest/home/js/templates.js
+++ scrollmagictest/home/js/templates.js
@@ -4,5 +4,5 @@ this["MyApp"]["templates"]["App"] = Handlebars.template({"compiler":[7,">= 4.0.0
     return "<div class=\"loading\">\r\n    <a>\r\n        give me some allowance\r\n    </a>\r\n</div>";
 },"useData":true});
 this["MyApp"]["templates"]["Loading"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<link href=\"css/loading.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n<div class=\"loading\">\r\n    <div class=\"loading_box\">\r\n        <div class=\"circle\"></div>\r\n        <div class=\"circle\"></div>\r\n        <div class=\"circle\"></div>\r\n    </div>\r\n</div>\r\n";
+    return "<link href=\"css/loading.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n<div class=\"loading\">\r\n    <div class=\"loading_box\">\r\n        <div class=\"svg-box\">\r\n            <object data=\"css/svg/line2.svg\" style=\"position:absolute; top: 6px;left: -10px;\"\r\n                type=\"image/svg+xml\"\r\n                codebase=\"http://www.adobe.com/svg/viewer/install/\" />\r\n        </div>\r\n    </div>\r\n</div>\r\n";
 },"useData":true});
