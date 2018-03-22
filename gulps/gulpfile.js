a768c4784f1e2f25f0c0ac68ce4e0e1d51b244b2
/ </>
2018-03-22 15:39:14 +0800

    improve

--- gulps/gulpfile.js
+++ gulps/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'), //本地安装gulp所用到的地方
     less = require('gulp-less'),
     htmlmin = require('gulp-htmlmin');
-
+//http://www.ydcss.com/archives/20
 gulp.task('Loading', function () {
     var options = {
         removeComments: true,
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
     };
     gulp.src('./../html/loading/loading.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
